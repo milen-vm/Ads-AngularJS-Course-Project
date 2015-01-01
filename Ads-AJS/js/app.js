d2c3864bc467d1e2f9f0c730caaf5498cfe9db15
milen-vm <bay_milen@abv.bg>
2015-01-01 19:14:00 +0200

    I've got responding login form

--- Ads-AJS/js/app.js
+++ Ads-AJS/js/app.js
@@ -13,33 +13,12 @@ app.config(function($routeProvider) {
         controller: 'RegistrationForm'
     });
     
+    $routeProvider.when('/login-form', {
+        templateUrl: 'templates/login-form.html',
+        // controller: 'RegistrationForm'
+    });
+    
     $routeProvider.otherwise({
         redirectTo: '/'
     });
 });
-
-app.directive('passwordMatch', [function () {   // http://rogeralsing.com/2013/08/26/angularjs-directive-to-check-that-passwords-match-followup/
-    return {
-        restrict: 'A',
-        scope:true,
-        require: 'ngModel',
-        link: function (scope, elem , attrs,control) {
-            var checker = function () {
- 
-                //get the value of the first password
-                var firstPassword = scope.$eval(attrs.ngModel);
- 
-                //get the value of the other password 
-                var secondPassword = scope.$eval(attrs.passwordMatch);
-                return firstPassword == secondPassword;
-            };
-            
-            scope.$watch(checker, function (n) {
- 
-                //set the form control to valid if both
-                //passwords are the same, else invalid
-                control.$setValidity('passwordmatch', n);
-            });
-        }
-    };
-}]);
---
+++ Ads-AJS/js/services/user-data.js
@@ -0,0 +1,9 @@
+'use strict';
+
+app.factory('userData', function() {
+    var baseUrl = 'http://softuni-ads.azurewebsites.net/api/user/';
+    
+    function userDataRequest(url, data) {
+        
+    }
+});

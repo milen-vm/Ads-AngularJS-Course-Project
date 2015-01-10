5a7115e73f8263b8ea477cb9671d8f70fe0d86ca
milen-vm <bay_milen@abv.bg>
2015-01-11 01:24:26 +0200

    Sorting users table.

--- Ads-AJS/js/controlers/admin/admin-users-list.js
+++ Ads-AJS/js/controlers/admin/admin-users-list.js
@@ -2,13 +2,14 @@
 
 app.controller('AdminUsersList', ['$scope', '$rootScope', 'adminUsers',
     function($scope, $rootScope, adminUsers) {
-        
         $scope.usersPerPage = 10;
         $scope.currentPage = 1;
         $scope.pagerMaxSize = 5;        
         $scope.sortUsersBy = '';
+        $scope.reverseSort = false;
         
         $scope.loadUsers = function() {
+            console.log($scope.sortUsersBy);
             adminUsers.getAll($scope.sortUsersBy, $scope.currentPage, $scope.usersPerPage).then(
                 function(data) {
                     $scope.totalUsers = data.numItems;
@@ -18,6 +19,16 @@ app.controller('AdminUsersList', ['$scope', '$rootScope', 'adminUsers',
                     $scope.errorOccurred(error.message);
                 }
             );
+        };
+        
+        $scope.sortByPropertyClicked = function(userProperty) {
+            if ($scope.reverseSort) {
+                $scope.sortUsersBy = '-' + userProperty;
+            } else {
+                $scope.sortUsersBy = userProperty;
+            };
+            
+            $scope.loadUsers();
         };
         
         // Event
